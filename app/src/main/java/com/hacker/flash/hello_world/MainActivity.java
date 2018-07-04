5210014d5e302aa4fd580f2e0242115d8e3acf99
sunyiming <sunyiming@qiyi.com>
2018-07-04 17:53:26 +0800

    手机节奏震动

--- app/src/main/java/com/hacker/flash/hello_world/MainActivity.java
+++ app/src/main/java/com/hacker/flash/hello_world/MainActivity.java
@@ -1,13 +1,80 @@
 package com.hacker.flash.hello_world;
 
+import android.app.Service;
+import android.content.Context;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn_hasVibrator;
+    private Button btn_short;
+    private Button btn_long;
+    private Button btn_rhythm;
+    private Button btn_cancle;
+    private Vibrator myVibrator;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //获得系统的Vibrator实例:
+        myVibrator = (Vibrator) getSystemService(Service.VIBRATOR_SERVICE);
+        mContext = MainActivity.this;
+        bindViews();
+    }
+
+    private void bindViews() {
+        btn_hasVibrator = (Button) findViewById(R.id.btn_hasVibrator);
+        btn_short = (Button) findViewById(R.id.btn_short);
+        btn_long = (Button) findViewById(R.id.btn_long);
+        btn_rhythm = (Button) findViewById(R.id.btn_rhythm);
+        btn_cancle = (Button) findViewById(R.id.btn_cancle);
+
+        btn_hasVibrator.setOnClickListener(this);
+        btn_short.setOnClickListener(this);
+        btn_long.setOnClickListener(this);
+        btn_rhythm.setOnClickListener(this);
+        btn_cancle.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_hasVibrator:
+                Toast.makeText(mContext, myVibrator.hasVibrator() ? "当前设备有振动器" : "当前设备无振动器",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_short:
+                myVibrator.cancel();
+                myVibrator.vibrate(new long[]{100, 200, 100, 200}, 0);
+                Toast.makeText(mContext, "短振动", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_long:
+                myVibrator.cancel();
+                myVibrator.vibrate(new long[]{100, 10000}, 0);
+                Toast.makeText(mContext, "长振动", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_rhythm:
+                myVibrator.cancel();
+                myVibrator.vibrate(new long[]{1047,
+                        100, 527, 100, 599, 100, 554, 100, 548, 100, 220, 100, 208, 100, 239, 100, 280, 500, 712,
+                        100, 528, 100, 532, 100, 547, 100, 489, 100, 197, 100, 273, 100, 586, 100, 528,
+                        100, 231, 100, 253, 300, 326, 100, 192, 100, 238, 300, 376, 100, 560, 200, 122, 100, 226, 300, 281, 400, 508,
+                        100, 279, 100, 480, 300, 297, 100, 222, 100, 212, 100, 239, 500, 116, 100, 202, 100, 272, 100, 207, 100, 240, 100}, -1);
+                Toast.makeText(mContext, "节奏振动", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_cancle:
+                myVibrator.cancel();
+                Toast.makeText(mContext, "取消振动", Toast.LENGTH_SHORT).show();
+        }
     }
 }
